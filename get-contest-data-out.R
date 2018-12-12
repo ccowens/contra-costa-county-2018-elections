e1d538a48f8b0f64c7ce767426a48dd3bdc0cb17
Charley Cowens <charley.cowens@gmail.com>
2018-12-12 14:51:20 -0800

    Update get-contest-data-out.R
    added in comments

--- get-contest-data-out.R
+++ get-contest-data-out.R
@@ -1,33 +1,49 @@
-if(!require(stringr)) {install.packages("stringr"); library(stringr)}
-if(!require(dplyr)) {install.packages("dplyr"); library(dplyr)}
-if(!require(readr)) {install.packages("readr"); library(readr)}
+if(!require(stringr)) {install.packages("stringr"); library(stringr)} #str_split str_trim str_sub
+if(!require(readr)) {install.packages("readr"); library(readr)} #write_csv
 
+# get, open, and read in the file with results as text
 download.file("https://results.enr.clarityelections.com/CA/Contra_Costa/92672/224337/reports/detailtxt.zip", "detailtxt.zip")
 unzip("detailtxt.zip")
 result_lines <- readLines("detail.txt")
+
+# mark the beginnings and ends of the lines for the contests 
 begins <- grep("Vote For", result_lines)
 ends <- grep("Totals:", result_lines)
+
+# parse the beginning lines for contest names and how many picks for each contest
 info_lines <- result_lines[begins] 
 split_lines <- matrix(unlist(str_split(info_lines, "\\(Vote For ")), ncol=2, byrow=TRUE)
 Contest <- str_trim(split_lines[,1],side="both")
 Select <- str_sub(split_lines[,2],-2,1)
-all_contests <- data.frame(Filename = paste0(1:length(info_lines),".csv"), Contest = Contest, Select = Select)
-write_csv(all_contests,"contests-with-filenames.csv")
 
+# create a numeric filename to reference the contest and write it to a CSV file with the other contest info
+all_contests <- data.frame(Filename = paste0(1:length(info_lines),".csv"), Contest = Contest, Select = Select)
+write_csv(all_contests,"index-of-contests.csv")
+
+# loop through and parse each set of lines for each contest 
 for (i in 1:length(all_contests[,1])) {
- print(i)
- writeLines(result_lines[(begins[i]+1):(ends[i]-1)], "temp.txt")
- columns <- nchar(readLines("temp.txt")[3])/30
- contest <- read.fwf("temp.txt", rep.int(30, columns))
- contest[] <- lapply(contest, as.character)
- colnames(contest) <- c(contest[1,])
- colnames(contest) <- str_trim(colnames(contest), side = "both")
- contest <- contest[,c(seq(1, columns-1, 2))]
- contest <- contest[-c(1,2),]
- rownames(contest) <- contest[,1]
- contest <- contest[,-1, drop=FALSE]
- contest[] <- lapply(contest, as.numeric)
- contest <- cbind(Precinct = rownames(contest), contest)
- rownames(contest) <- NULL
- write_csv(contest, paste0(i,".csv"))
+  print(i) #echo
+  # save out the text lines for the contest and read in as a fixed width file 
+  writeLines(result_lines[(begins[i]+1):(ends[i]-1)], "temp.txt")
+  columns <- nchar(readLines("temp.txt")[3])/30 #columns are eaually 30 wide
+  contest <- read.fwf("temp.txt", rep.int(30, columns))
+  # make everything character
+  contest[] <- lapply(contest, as.character)
+  # add candidate/choice names as column names and clean them
+  colnames(contest) <- c(contest[1,])
+  colnames(contest) <- str_trim(colnames(contest), side = "both")
+  # drop duplicative columns and last column, then drop top two rows
+  contest <- contest[,c(seq(1, columns-1, 2))]
+  contest <- contest[-c(1,2),]
+  # save the precinct names as row names
+  rownames(contest) <- contest[,1]
+  # drop the precinct column so everything remaining as data should be a number, but stay as a data frame
+  contest <- contest[,-1, drop=FALSE]
+  # make the data numeric
+  contest[] <- lapply(contest, as.numeric)
+  # add the Precinct column back in, trim off white space, and throw out the row names
+  contest <- cbind(Precinct = str_trim(rownames(contest), side = "both"), contest)
+  rownames(contest) <- NULL
+  # write everything out
+  write_csv(contest, paste0(i,".csv"))
 }
