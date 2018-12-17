98bb8a23344648b35b266309765993ab048e5293
Charley Cowens <charley.cowens@gmail.com>
2018-12-17 00:09:41 -0800

    added suffix handling for names

--- ananlyze-contest.R
+++ ananlyze-contest.R
@@ -11,10 +11,29 @@ wccusd <- read_csv("contests/32.csv")
 
 full <- colnames(wccusd)
 partial <- c(full[-1])
-partial <- c("CHARLES COWENS", "JOE BLOW, JR.")
-str_split((str_split(partial, ", ")), " ")
-
+partial <- sapply(str_split(partial, " "), function (x) if(x[length(x)] %in% c("JR", "JR.", "III", "IV", "V")) 
+    {x[length(x)-1]}
+  else
+    {x[length(x)]})
+colnames(wccusd)[-1] <- partial
+  
 wccusd$City_code <- substr(wccusd$Precinct, 1, 4)
-wccusd <- left_join(wccusd, map_to_cities) %>% select(-City_code, -Precinct) %>% select(City, everything())
+wccusd <- left_join(wccusd, map_to_cities) %>% 
+  select(-City_code, -Precinct) %>% 
+  select(City, everything())
 
-adorn_pct_formatting(adorn_percentages(aggregate(. ~ City, wccusd, sum)))
+wccusd <- aggregate(. ~ City, wccusd, sum)
+
+wccusd <- adorn_percentages(wccusd)
+
+x <- apply(wccusd[-1], 2, max)
+
+ <- t(data.frame(x))
+
+
+join(y, wccusd)
+
+str(data.frame(t(t(x))))
+t(t(x))
+left_join(x, wccusd)
+x[]
