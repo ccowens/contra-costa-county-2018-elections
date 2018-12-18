542b4c0e4bba9fcb664b2a5cccc35d8d2497891b
Charley Cowens <charley.cowens@gmail.com>
2018-12-17 16:09:03 -0800

    added best city for candidate

--- ananlyze-contest.R
+++ ananlyze-contest.R
@@ -1,6 +1,7 @@
 if(!require(readr)) {install.packages("readr"); library(readr)}
 if(!require(dplyr)) {install.packages("dplyr"); library(dplyr)}
 if(!require(janitor)) {install.packages("janitor"); library(janitor)}
+if(!require(stringr)) {install.packages("stringr"); library(stringr)}
 
 map_to_cities <- data.frame(City_code = c("ELCR","ELSO","ERIC","GIAN","HERC","KENS","NRIC","PINL","RICH","ROLL","SPAB"),
                             City = c("El Cerrito","El Sobrante","East Richmond Heights","Richmond","Hercules","Kensington","North Richmond","Pinole","Richmond","Richmond","San Pablo"),
@@ -9,13 +10,13 @@ map_to_cities <- data.frame(City_code = c("ELCR","ELSO","ERIC","GIAN","HERC","KE
 
 wccusd <- read_csv("contests/32.csv")
 
-full <- colnames(wccusd)
-partial <- c(full[-1])
-partial <- sapply(str_split(partial, " "), function (x) if(x[length(x)] %in% c("JR", "JR.", "III", "IV", "V")) 
+fullrow <- colnames(wccusd)
+candidates <- c(fullrow[-1])
+candidates <- sapply(str_split(candidates, " "), function (x) if(x[length(x)] %in% c("JR", "JR.", "III", "IV", "V")) 
     {x[length(x)-1]}
   else
     {x[length(x)]})
-colnames(wccusd)[-1] <- partial
+colnames(wccusd)[-1] <- candidates
   
 wccusd$City_code <- substr(wccusd$Precinct, 1, 4)
 wccusd <- left_join(wccusd, map_to_cities) %>% 
@@ -26,14 +27,10 @@ wccusd <- aggregate(. ~ City, wccusd, sum)
 
 wccusd <- adorn_percentages(wccusd)
 
-x <- apply(wccusd[-1], 2, max)
+best_result <- apply(wccusd[-1], 2, max)
 
- <- t(data.frame(x))
+best_city <- unlist(lapply(candidates, function(x) {
+  wccusd[match(best_result[x], pull(select(wccusd,x))), 1]
+}))
 
-
-join(y, wccusd)
-
-str(data.frame(t(t(x))))
-t(t(x))
-left_join(x, wccusd)
-x[]
+best_city_by_candidate <- data.frame(`Best Result` = best_result, `City` = best_city, stringsAsFactors = FALSE)
